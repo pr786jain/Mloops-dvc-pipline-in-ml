7075b598f06600a10496f489307b1767a59f05c3
komal jain <komaljain2530@gmail.com>
2025-04-30 19:18:23 +0530

    data preprocessing

--- src/data_ingenstion.py
+++ src/data_ingenstion.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np 
 import os 
 from sklearn.model_selection import train_test_split
-
+from pathlib import Path
 import logging
 
 log_dir ='logs'
@@ -35,16 +35,16 @@ def load_data(data_url: str)-> pd.DataFrame:
         df = pd.read_csv(data_url)
         logger.debug('Data loaded from %s',data_url)
         return df 
-    except pd.errors.ParseError as e:
+    except pd.errors.ParserError as e:
         logger.error('Failed to parse the csv file %s',e)
         raise  
         
 def save_data(train_data:pd.DataFrame , test_data: pd.DataFrame, data_path: str)->None:
     try:
-        raw_data_path =os.path(data_path,'raw')
-        os.makedirs(raw_data_path,exit_ok=True)
+        raw_data_path =os.path.join(data_path,'raw')
+        os.makedirs(raw_data_path,exist_ok=True)
         train_data.to_csv(os.path.join(raw_data_path, "train.csv"),index=False);
-        test.data.to.csv(os.path.join(raw_data_path, "test.csv"),index=False);
+        test_data.to_csv(os.path.join(raw_data_path, "test.csv"),index=False);
         logger.debug('Train and test data saved to %s', raw_data_path)
     except Exception as e:
         logger.error('Unexpected error occurred while saving the data: %s',e)
@@ -54,4 +54,17 @@ def save_data(train_data:pd.DataFrame , test_data: pd.DataFrame, data_path: str)
 def main():
     try:
         test_size = 0.2
-        data_path =''
+        data_path = Path('C:\MLoops Pipline\spam_or_not_spam .csv')
+        df = load_data(data_url = data_path)
+
+        train_data ,test_data =train_test_split(df, test_size=test_size , random_state = 2)
+        
+        save_data(train_data ,test_data ,data_path='./data')
+    
+    except Exception as e:
+        logger.error('Failed to completer the data integeration process %s' ,e)
+        print(f"Error: {e}")
+        
+
+if __name__ =='__main__':
+    main()        
